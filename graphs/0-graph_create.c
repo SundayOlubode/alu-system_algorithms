0a117e237b6137abb1bd32b51a30ca9663f0ae8c
Sam <s.olubode@alustudent.com>
2024-09-16 08:42:54 +0100

    0-graph_create.c

---
+++ graphs/0-graph_create.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "graphs.h"
+
+graph_t *graph_create(void)
+{
+        graph_t *graph = malloc(sizeof(graph_t));
+        vertex_t *vertices;
+
+        if (graph == NULL)
+        {
+                return (NULL);
+        }
+
+        graph->nb_vertices = NULL;
+        graph->vertices = vertices;
+}
