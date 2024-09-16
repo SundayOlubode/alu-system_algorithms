401a3f69e47276122e5ad01d32bd69ee4c6cb219
Sam <s.olubode@alustudent.com>
2024-09-16 08:57:36 +0100

    0-graph_create.c

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include "graphs.h"
 
+/**
+ * graph_create - creates a grapgh and initialize it.
+ * Return: pointer to a graph.
+ */
 graph_t *graph_create(void)
 {
         graph_t *graph = malloc(sizeof(graph_t));
