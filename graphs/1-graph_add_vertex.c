8fb3f52ebec6abbb3eb6348be956b1334b6eddd8
Sam <s.olubode@alustudent.com>
2024-09-16 10:01:38 +0100

    1-graph_add_vertex.c

---
+++ graphs/1-graph_add_vertex.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "graphs.h"
+
+vertex_t *graph_add_new_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *new_vertex = malloc(sizeof(vertex_t));
+	vertex_t *g_vertex = graph->vertices;
+
+	if (new_vertex == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int i = 0; i < graph->nb_vertices; i++)
+	{
+		if (*(g_vertex->content) == *str)
+		{
+			free(g_vertex);
+			return (NULL);
+		}
+		g_vertex++;
+	}
+
+	strcpy(new_vertex->content, str);
+	new_vertex->edges = NULL;
+	new_vertex->nb_edges = 0;
+
+	vertex_t *new_vertices = realloc(graph->vertices, (graph->nb_vertices + 1) * sizeof(vertex_t));
+	if (new_vertices == NULL)
+	{
+		free(new_vertex->content);
+		free(new_vertex);
+		return (NULL);
+	}
+
+	graph->vertices = new_vertices;
+	graph->vertices[graph->nb_vertices++] = *new_vertex;
+
+	return (new_vertex);
+}
