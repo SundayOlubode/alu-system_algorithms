fe741b04d5550c5dc2d112fdfe2068d809e4cb46
Sam <s.olubode@alustudent.com>
2024-09-17 08:32:16 +0100

    2-graph_add_edge.c

---
+++ graphs/2-graph_add_edge.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include "graphs.h"
+
+/**
+ * graph_add_edge - adds an edge to two vertices in the given graph.
+ * @graph: the graph.
+ * @src: the string identifying the vertex to make the connection from.
+ * @dest: the string identifying the vertex to connect to.
+ * @type: the type of edge.
+ *
+ * Return: 1 | 0
+ */
+int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type)
+{
+}
