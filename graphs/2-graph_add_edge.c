47a5a2e38c94b8bb0e8aeea4c7893bf2e94d02f4
Sam <s.olubode@alustudent.com>
2024-09-20 15:00:03 +0100

    2-graph_add_edge.c

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "graphs.h"
 
+vertex_t *get_vertex(graph_t *graph, const char *src);
+
 /**
  * graph_add_edge - adds an edge to two vertices in the given graph.
  * @graph: the graph.
@@ -13,4 +15,109 @@
  */
 int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type)
 {
+        vertex_t *src_vertex, *dest_vertex;
+        edge_t *edge;
+
+        if (graph == NULL || src == NULL || dest == NULL)
+        {
+                return (0);
+        }
+        if (type != UNIDIRECTIONAL && type != BIDIRECTIONAL)
+        {
+                return (0);
+        }
+
+        /*find src vertex*/
+        src_vertex = get_vertex(graph, src);
+        if (src_vertex == NULL)
+        {
+                return (0);
+        }
+
+        /*find dest vertex*/
+        dest_vertex = get_vertex(graph, dest);
+        if (dest_vertex == NULL)
+        {
+                return (0);
+        }
+
+        /*connect edge*/
+        edge = create_edge(src_vertex, dest_vertex, type);
+        if (edge == NULL)
+        {
+                return (0);
+        }
+
+        src_vertex->nb_edges++;
+        dest_vertex->nb_edges++;
+
+        return (1);
+}
+
+/**
+ * get_vertex - get a vertex with the content string.
+ *
+ * @graph: grapgh to traverse.
+ * @str: string to search for.
+ *
+ * Return: vertex | NULL.
+ */
+vertex_t *get_vertex(graph_t *graph, const char *str)
+{
+        vertex_t *g_vertex = graph->vertices;
+
+        while (g_vertex != NULL)
+        {
+                if (strcmp(g_vertex->content, str) == 0)
+                {
+                        return (g_vertex);
+                }
+        }
+
+        free(g_vertex);
+        return (NULL);
+}
+
+/**
+ * create_edge - creates an edge
+ *
+ * @src_vertex: source vertex.
+ * @dest_vertex: destination vertex.
+ * @type: BIDIRECTIONAL | UNIDIRECTIONAL.
+ *
+ * Return: egde pointer | NULL.
+ */
+edge_t *create_edge(vertex_t *src_vertex,
+                    vertex_t *dest_vertex, edge_type_t type)
+{
+        edge_t *edge = malloc(sizeof(edge_t));
+
+        if (edge == NULL)
+        {
+                return (NULL);
+        }
+
+        edge->dest = dest_vertex;
+        edge->next = NULL;
+
+        if (type == BIDIRECTIONAL)
+        {
+
+                edge_t *reverse_edge = malloc(sizeof(edge_t));
+
+                if (reverse_edge == NULL)
+                {
+                        free(edge);
+                        return (NULL);
+                }
+
+                reverse_edge->dest = src_vertex;
+                reverse_edge->next = dest_vertex->edges;
+                dest_vertex->edges = reverse_edge;
+        }
+
+        edge->next = src_vertex->edges;
+        src_vertex->edges = edge;
+
+        return (edge);
 }
