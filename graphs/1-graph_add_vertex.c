53d195821025a59b1808e4d839b5340283104335
Sam <s.olubode@alustudent.com>
2024-09-21 10:08:28 +0100

    1-graph_add_vertex.c

--- graphs/1-graph_add_vertex.c
+++ graphs/1-graph_add_vertex.c
@@ -12,44 +12,37 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
 	vertex_t *new_vertex = malloc(sizeof(vertex_t));
 	vertex_t *g_vertex = graph->vertices;
-	vertex_t *prev_g_vertex;
 
-	if (graph == NULL)
+	if (graph == NULL || str == NULL)
 		return (NULL);
-	if (str == NULL)
-		return (NULL);
+
 	if (new_vertex == NULL)
 		return (NULL);
-	while (g_vertex != NULL)
-	{
-		if (strcmp(g_vertex->content, str) == 0)
-		{
-			free(new_vertex);
-			return (NULL);
-		}
-		prev_g_vertex = g_vertex;
-		g_vertex = prev_g_vertex->next;
-	}
+
 	new_vertex->content = strdup(str);
 	if (new_vertex->content == NULL)
 	{
 		free(new_vertex);
 		return (NULL);
 	}
-	new_vertex->index = 0;
+
 	new_vertex->edges = NULL;
 	new_vertex->nb_edges = 0;
+	new_vertex->index = graph->nb_vertices;
 	new_vertex->next = NULL;
-	if (graph->nb_vertices == 0)
+
+	if (graph->vertices == NULL)
 	{
 		graph->vertices = new_vertex;
 	}
 	else
 	{
-		prev_g_vertex->next = new_vertex;
-		new_vertex->index = prev_g_vertex->index + 1;
+		g_vertex = graph->vertices;
+		while (g_vertex->next != NULL)
+			g_vertex = g_vertex->next;
+		g_vertex->next = new_vertex;
 	}
-	graph->nb_vertices++;
 
+	graph->nb_vertices++;
 	return (new_vertex);
 }
