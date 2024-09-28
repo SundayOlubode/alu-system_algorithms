69a3555e01709be2318e10d434364162c25d3f11
Sam <s.olubode@alustudent.com>
2024-09-28 14:47:33 +0100

    huffman_tree.c

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -13,20 +13,20 @@
  */
 void freeNestedNode(void *data)
 {
-        binary_tree_node_t *node_data = NULL;
-        symbol_t *symbol_data = NULL;
+	binary_tree_node_t *node_data = NULL;
+	symbol_t *symbol_data = NULL;
 
-        node_data = (binary_tree_node_t *)data;
+	node_data = (binary_tree_node_t *)data;
 
-        if (node_data)
-        {
-                symbol_data = (symbol_t *)(node_data->data);
+	if (node_data)
+	{
+		symbol_data = (symbol_t *)(node_data->data);
 
-                if (symbol_data)
-                        free(symbol_data);
+		if (symbol_data)
+			free(symbol_data);
 
-                free(node_data);
-        }
+		free(node_data);
+	}
 }
 
 /**
@@ -41,29 +41,29 @@ void freeNestedNode(void *data)
  */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
-        heap_t *priority_queue = NULL;
-        binary_tree_node_t *huffman_root = NULL;
+	heap_t *priority_queue = NULL;
+	binary_tree_node_t *huffman_root = NULL;
 
-        if (!data || !freq || size == 0)
-                return (NULL);
+	if (!data || !freq || size == 0)
+		return (NULL);
 
-        priority_queue = huffman_priority_queue(data, freq, size);
+	priority_queue = huffman_priority_queue(data, freq, size);
 
-        if (!priority_queue)
-                return (NULL);
+	if (!priority_queue)
+		return (NULL);
 
-        while (priority_queue->root &&
-               (priority_queue->root->left || priority_queue->root->right))
-        {
-                if (!huffman_extract_and_insert(priority_queue))
-                {
-                        heap_delete(priority_queue, freeNestedNode);
-                        return (NULL);
-                }
-        }
+	while (priority_queue->root &&
+	       (priority_queue->root->left || priority_queue->root->right))
+	{
+		if (!huffman_extract_and_insert(priority_queue))
+		{
+			heap_delete(priority_queue, freeNestedNode);
+			return (NULL);
+		}
+	}
 
-        huffman_root = (binary_tree_node_t *)(priority_queue->root->data);
-        heap_delete(priority_queue, NULL);
+	huffman_root = (binary_tree_node_t *)(priority_queue->root->data);
+	heap_delete(priority_queue, NULL);
 
-        return (huffman_root);
+	return (huffman_root);
 }
