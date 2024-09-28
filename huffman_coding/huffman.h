a8e6c4b060cbe61050c11864f483e9f4c85ef747
Sam <s.olubode@alustudent.com>
2024-09-28 14:54:33 +0100

    huffman_codes.c

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -11,8 +11,8 @@
  */
 typedef struct symbol_s
 {
-    char data;
-    size_t freq;
+        char data;
+        size_t freq;
 } symbol_t;
 
 symbol_t *symbol_create(char data, size_t freq);
@@ -20,5 +20,6 @@ heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
 int huffman_codes(char *data, size_t *freq, size_t size);
+void symbol_delete(void *symbol);
 
 #endif
