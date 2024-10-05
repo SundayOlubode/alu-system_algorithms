47edc306b10d9ade72cc7a8440015ffe3389ca5d
Sam <s.olubode@alustudent.com>
2024-10-05 15:45:32 +0100

    1-backtracking_array.c

---
+++ pathfinding/2-backtracking_array.c
@@ -0,0 +1,83 @@
+#include "pathfinding.h"
+#include <sys/types.h>
+
+static int *dists;
+static vertex_t **from;
+static vertex_t **verts;
+
+#define STRDUP(x) ((str = strdup(x)) ? str : (exit(1), NULL))
+
+/**
+ * dijkstra_graph - Implements Dijkstra's algorithm to find the shortest path
+ * from the start vertex to the target vertex in a graph.
+ * @graph: Pointer to the graph structure.
+ * @start: Pointer to the starting vertex.
+ * @target: Pointer to the target vertex.
+ *
+ * Return: A queue of vertices representing the path, or NULL if no path exists.
+ */
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+			vertex_t const *target)
+{
+	ssize_t i, d, j = -1;
+	vertex_t *v;
+	edge_t *e;
+	queue_t *path = queue_create();
+	char *str;
+
+	if (!graph || !start || !target || !path)
+		return (NULL);
+
+	/* Allocate memory for distance and vertex tracking arrays */
+	dists = calloc(graph->nb_vertices, sizeof(*dists));
+	from = calloc(graph->nb_vertices, sizeof(*from));
+	verts = calloc(graph->nb_vertices, sizeof(*verts));
+
+	if (!dists || !from || !verts || !path)
+		return (NULL);
+
+	/* Initialize distances and vertices */
+	for (v = graph->vertices; v; v = v->next)
+		verts[v->index] = v, dists[v->index] = INT_MAX;
+
+	dists[start->index] = 0, from[start->index] = NULL;
+
+	/* Main loop to find the shortest path using Dijkstra's algorithm */
+	while (j != (ssize_t)target->index)
+	{
+		/* Find the vertex with the smallest distance */
+		for (d = INT_MAX, j = -1, i = 0; i < (ssize_t)graph->nb_vertices; i++)
+			if (dists[i] >= 0 && dists[i] < d)
+				d = dists[i], j = i;
+
+		/* If no more vertices to process, break */
+		if (j == -1)
+			break;
+
+		printf("Checking %s, distance from %s is %d\n",
+		       verts[j]->content, start->content, dists[j]);
+
+		/* Update distances for neighboring vertices */
+		for (e = verts[j]->edges; e; e = e->next)
+			if (dists[e->dest->index] >= 0 &&
+			    dists[j] + e->weight < dists[e->dest->index])
+				dists[e->dest->index] = dists[j] + e->weight,
+				from[e->dest->index] = verts[j];
+
+		/* Mark the current vertex as processed */
+		dists[j] = -1;
+	}
+
+	/* If target was reached, build the path */
+	if (j != -1)
+		for (queue_push_front(path, STRDUP(verts[j]->content));
+		     j != (ssize_t)start->index; j = from[j]->index)
+			queue_push_front(path, STRDUP(from[j]->content));
+	else
+		path = (free(path), NULL);
+
+	/* Free allocated memory */
+	free(dists), free(from), free(verts);
+
+	return (path);
+}
